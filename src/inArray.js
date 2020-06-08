da0ea96b931e54b631c48b22ca776fa45fb71ccd
pasc2018kerc2019 <folgofirst@gmail.com>
2020-06-08 20:59:54 +0200

    arr ok

--- src/inArray.js
+++ src/inArray.js
@@ -5,8 +5,29 @@
  * ary = ["2d", "5s", "Ah"] val = "Ah" alors inArray(ary, val) retournera true
  * ary = ["2d", "5s", "Ah"] val = "2h" alors inArray(ary, val) retournera false
  */
+// function inArray(ary, val) {
+
+//   if(val in ary == ary.includes(val)){
+//     return true
+//   }
+//   else {
+//     return false
+//   }
+// }
+
+// export { inArray };
+
 function inArray(ary, val) {
-  // CODE HERE
+
+ if (ary.indexOf(val) != null){
+   return true;
+ }
+else {
+  return false;
 }
 
+
+
+
+}
 export { inArray };
