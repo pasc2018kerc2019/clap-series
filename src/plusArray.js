850c2bc8364f351517dd86c102f92fbbdea990b4
pasc2018kerc2019 <folgofirst@gmail.com>
2020-06-08 20:59:20 +0200

    to fix

--- src/plusArray.js
+++ src/plusArray.js
@@ -5,8 +5,16 @@
  * et arr2 = [5, 4, 3]
  * alors plusArray(arr1, arr2) retournera 30 
  */
+
+ /*mettre en place  fonction reduce()
+ */
+
 function plusArray(arr1, arr2) {
-  // CODE HERE
+ //reduce() applique une fonction qui est un « accumulateur » et qui traite chaque valeur d'une liste (de la gauche vers la droite) afin de la réduire à une seule valeur.
+  
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr1.reduce(reducer)+arr2.reduce(reducer)
 }
 
 export { plusArray };
